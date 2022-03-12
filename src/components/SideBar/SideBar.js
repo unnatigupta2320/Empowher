e26db20a45a7b4f80c00cf5da889fc1ff1c91092
Unnati Gupta <unnati.2320@gmail.com>
2022-03-12 19:14:16 +0530

    added the sidebar menu

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from "react-pro-sidebar";
+import { FaList, FaRegHeart } from "react-icons/fa";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { RiPencilLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
+import "react-pro-sidebar/dist/css/styles.css";
+import "./../../styling/Header.css";
+import { Link } from "react-router-dom";
+
+
+const SideBar = () => {
+
+    //create initial menuCollapse state using useState hook
+    const [menuCollapse, setMenuCollapse] = useState(false)
+
+    //create a custom function that will change menucollapse state from false to true and true to false
+    const menuIconClick = () => {
+        //condition checking to change state from true to false and vice versa
+        menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+    };
+
+    return (
+        <>
+            <div id="header">
+                {/* collapsed props to change menu size using menucollapse state */}
+                <ProSidebar collapsed={menuCollapse}>
+                    <SidebarHeader>
+                        <div className="logotext">
+                            {/* small and big change using menucollapse state */}
+                            <p>{menuCollapse ? "Her" : "EmpowHer"}</p>
+                        </div>
+                        <div className="closemenu" onClick={menuIconClick}>
+                            {/* changing menu collapse icon on click */}
+                            {menuCollapse ? (
+                                <FiArrowRightCircle />
+                            ) : (
+                                <FiArrowLeftCircle />
+                            )}
+                        </div>
+                    </SidebarHeader>
+                    <SidebarContent>
+                        <Menu iconShape="square">
+                            <MenuItem active={true} icon={<FiHome />}>
+                                <li className='nav-item'>
+                                    <Link to='/' className='nav-links'>
+                                        Home
+                                    </Link>
+                                </li>
+                            </MenuItem>
+                            <MenuItem icon={<FaList />}>
+                            <li className='nav-item'>
+                                    <Link to='/about' className='nav-links'>
+                                        About Us
+                                    </Link>
+                                </li>
+                            </MenuItem>
+                            <MenuItem icon={<FaRegHeart />}>
+                            <li className='nav-item'>
+                                    <Link to='/dashboard' className='nav-links'>
+                                        Dashboard
+                                    </Link>
+                                </li>
+                            </MenuItem>
+                            <MenuItem icon={<RiPencilLine />}>
+                            <li className='nav-item'>
+                                    <Link to='/profile' className='nav-links'>
+                                        Profile
+                                    </Link>
+                                </li>
+                            </MenuItem>
+                        </Menu>
+                    </SidebarContent>
+                    <SidebarFooter>
+                        <Menu iconShape="square">
+                            <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+                        </Menu>
+                    </SidebarFooter>
+                </ProSidebar>
+            </div>
+        </>
+    );
+};
+
+export default SideBar;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './components/HomePage.js';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './components/NavBar/Navbar.js'
+import Sidebar from './components/SideBar/SideBar.js';
+import './styling/index.css';
 
 const Routing = () => {
   return (
     <Router>
       <Switch>
-        <div>
-          <Navbar />
-          <Route path='/' exact component={HomePage} />
-          <Route path='/about' component={HomePage} />
-          <Route path='/products' component={HomePage} />
-          <Route path='/contact-us' component={HomePage} />
-          <Route path='/sign-up' component={HomePage} />
-          <Route path='/marketing' component={HomePage} />
-          <Route path='/consulting' component={HomePage} />
+        <div class="splitscreen">
+          <div class="left">
+            <Sidebar />
+          </div>
+          <div class="right">
+            <Route path='/' exact component={HomePage} />
+            <Route path='/about' component={HomePage} />
+            <Route path='/products' component={HomePage} />
+            <Route path='/contact-us' component={HomePage} />
+            <Route path='/sign-up' component={HomePage} />
+            <Route path='/marketing' component={HomePage} />
+            <Route path='/consulting' component={HomePage} />
+          </div>
         </div>
       </Switch>
     </Router>
